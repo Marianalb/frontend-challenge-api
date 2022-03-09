95e383576cf185d13c2f0785cc8c32a3a4cb35c9
Unknown <unknown@example.com>
2022-03-09 22:41:39 +0000

    create react project

---
+++ react/my-blog/src/components/app/App.jsx
@@ -0,0 +1,24 @@
+import Feed from '../feed/Feed';
+import PostDetails from '../post-details/PostDetails';
+import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+            <Route path="/" element={<Feed/>}/>
+            <Route path="/post-details/:postId" element={<PostDetails/>}/>
+        </Routes>
+      </Router>   
+    </div>
+  );
+}
+
+export default App;
